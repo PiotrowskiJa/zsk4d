55dbaee664e3952298502c1f4caf4e6b64f28170
Jan Piotrowski <jaspio3v32@gmail.com>
2019-09-09 09:34:47 +0200

    atom

---
+++ index3.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      $name = 'dzanek';
+      $name = 'janek';
+      $name = 'dzbanek';
+     ?>
+  </body>
+</html>
